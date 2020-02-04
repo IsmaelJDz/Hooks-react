7c52754f67b0b5dd55ab6b7540188966397e94d1
Rene Ismael <Ismael_br7@hotmail.com>
2020-02-04 00:37:38 -0600

    Custom hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, {useState, useEffect, Fragment } from 'react';
 import axios from "axios"
 
-function App() {
-
+const useHackerNewsApi = () => {
   const [data, setData] = useState({ hits: [] })
-  const [query, setQuery] = useState('redux')
   const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=redux')
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
@@ -29,18 +27,34 @@ function App() {
 
   }, [url])
 
+  return [{data, isLoading, isError}, setUrl]
+}
+
+function App() {
+
+  const [query, setQuery] = useState('redux')
+  const [{ data, isLoading, isError }, doFetch] = useHackerNewsApi();
+
   return (
     <Fragment>
-      <input 
-        type="text" 
-        value={query} 
-        onChange={event => setQuery(event.target.value)} 
-      />
-      <button 
-        type="button" 
-        onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}>
-          Search
-      </button>
+      <form 
+        onSubmit={event => {
+          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`)
+          event.preventDefault();
+        }}
+      >
+        <input 
+          type="text" 
+          value={query} 
+          onChange={event => setQuery(event.target.value)} 
+        />
+        {/* <button 
+          type="button" 
+          onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}>
+            Search
+        </button> */}
+        <button type="submit">Search</button>
+      </form>
       {isError && <div>Semething went wrong ...</div>}
       {isLoading ? (<div>Loading...</div>) : (      
         <ul>
